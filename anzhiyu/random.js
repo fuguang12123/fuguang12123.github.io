27b4714de722ea8f31deb4abd7377107e09e95d5
fuguang12123 <3326498228@qq.com>
2025-03-12 14:54:39 +0800

    Site updated: 2025-03-12 14:54:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/09/happyBirthday/","2025/03/09/heelo/"];function toRandomPost(){
+var posts=["2025/03/09/heelo/","2025/03/09/happyBirthday/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
